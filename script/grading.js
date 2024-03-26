9278f0d8c644a42df272130404c7d02d07e4f7f2
鈴木　舜基 <syunki555777@gmail.com>
2024-03-26 22:01:33 +0900

    プロジェクトワークスペース設定と基本的なフロントエンドファイルの追加
    このコミットでは、プロジェクトワークスペースのxmlファイルとそれに対応するvcs xmlファイルが導入されます。また、HTMLページ（index.html）の初期構造とそのCSSとJavaScriptファイルが追加されます。特筆すべきは、jQueryライブラリの含まれ、ページのボタン動作を処理する基本スクリプトが含まれていることです。

---
+++ script/grading.js
@@ -0,0 +1,10 @@
+let answers = [];
+console.log($("#buttons").children);
+for(var i = 0; i <= answers.length; i++){
+    $("#buttons").children("find")
+}
+$("#buttons").children("*").forEach((e) => {
+    answers.push($(e).attr("label"));
+});
+
+console.log(answer);
